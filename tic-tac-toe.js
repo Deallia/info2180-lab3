167698d966e6cbd9f4b5d07f7c6e856cea50a557
Deallia Dunbar <dealliadunbar@gmail.com>
2022-10-10 20:41:51 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,49 @@
-"use strict"
 
 
+
+let classX = document.createElement("p");
+classX.innerHTML='X';
+console.log(classX);
 
 window.onload = (event) => {
-var result = [], parent = document.getElementById("board");
-for (var i = 0, l = parent.childNodes.length; i < l; i++) 
-    result.push(parent.childNodes[i]);
-for (var i = 0, l = result.length; i < l; i++) 
-    
-	result[i].className="square";
-};
 
+var Board = Array.from(document.getElementById("board").children);
+console.log(Board);
+for (let i of Board){
+	i.classList.add("square");
+}
+var player;
+const playerX ="X";
+const playerO ="O";
+
+const wins=[
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,4,8],
+[2,4,6],
+[0,3,6],
+[1,4,7],
+[2,5,8]
+]
+
+const grid = () => Array.from(document.getElementsByClassName("square"));
+const enableListeners =() => grid().forEach(cell => cell.addEventListener("click", function(event){
+currentPlayer = player ? playerO : playerX;
+
+event.target.innerHTML=currentPlayer;
+event.target.classList.add(currentPlayer);
+
+
+nextPlayer();
+checkWin();
+}, {once:true}));
+
+function nextPlayer(){
+player=!player;
+}
+
+
+enableListeners();
+};
 
